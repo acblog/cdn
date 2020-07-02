9723cbd27517dab56106112af8c151163960cdce
StardustDL <34736356+StardustDL@users.noreply.github.com>
2020-07-02 11:48:09 +0000

    Deploying to cdn from  @ 6609a6e6f8f63772ea46d4dbb870762f34c7f397 🚀

---
+++ client/js/interop.js
@@ -0,0 +1,39 @@
+﻿window.acblogInteropSetTitle = function (newTitle) {
+    document.title = newTitle;
+};
+window.acblogInteropScrollTo = function (id) {
+    var scrollToElement = document.getElementById(id);
+    if (scrollToElement && scrollToElement.offsetTop) {
+        window.scrollTo(0, scrollToElement.offsetTop);
+    }
+};
+window.acblogInteropCopyItem = function (ele) {
+    ele.select();
+    document.execCommand('copy');
+}
+window.acblogInteropModalAction = function (id, action) {
+    var m = $('#' + id);
+    if (m) {
+        m.modal(action);
+    }
+};
+window.acblogInteropToastAction = function (id, action) {
+    var m = $('#' + id);
+    if (m) {
+        m.modal(action);
+    }
+};
+window.acblogInteropTooltipEnable = function () {
+    $('[data-toggle="tooltip"]').tooltip();
+};
+window.acblogInteropPopoverEnable = function () {
+    $('[data-toggle="popover"]').popover()
+};
+window.acblogInteropLoadingInfoShow = function () {
+    var loadingInfo = document.getElementById("loading-info-ui");
+    loadingInfo.style = "display: block";
+}
+window.acblogInteropLoadingInfoHide = function () {
+    var loadingInfo = document.getElementById("loading-info-ui");
+    loadingInfo.style = "display: none";
+}
